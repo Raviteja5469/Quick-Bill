512dcf1bd03aa6a4ec50a2c6266aba3f61dcd0c7
Jayendra <voutlajay8765@gmail.com>
2024-12-24 15:37:30 +0530

    updated components

---
+++ frontend/public/Components/FeatureList/FeatureList.jsx
@@ -0,0 +1,14 @@
+// this components contains the div which has all the features in a grid manner
+// each feature is a rectangular box with feature name and button
+
+import React from 'react'
+
+const FeatureList = () => {
+  return (
+    <div className='feature-list'>
+      
+    </div>
+  )
+}
+
+export default FeatureList
