4fe9fe4571c4c5daea220d073ad0fe1d6f4d470b
Jayendra <voutlajay8765@gmail.com>
2024-12-24 14:40:42 +0530

    updated navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import Footer from '../public/Components/Footer/Footer'
+import Navbar from '../public/Components/Navbar/Navbar'
 import './App.css'
 
 function App() {
 
+  // for now keep it like this later i will add routing
+
   return (
     <>
+    <Navbar/>
       hey this is the home page
+      <Footer/>
+
+
     </>
   )
 }
