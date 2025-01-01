765623e7eb2327857bdfe921265b2cf0c217de93
Raviteja5469 <ravitejaseguri@gmail.com>
2025-01-01 20:57:09 +0530

    updated hero

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -4,12 +4,15 @@ import './Hero.css'
 const Hero = () => {
   return (
     <div className="home" color='red'>
+      <img src="assets\hero2" alt="" />
       <div className="hero">
         <div className="hero-container">
-          <div className="hero_image">
-            <img src="assets\hero.png" alt="" />
+          <img className="hero_image_main" src={"https://img.freepik.com/free-photo/top-view-desk-concept-with-notepad_23-2148604955.jpg"} alt="" />
             <h1 className='hero-heading'>Explore Our Site for Hassle-Free Billing and Comprehensive Accounting Services</h1>
-          </div>
+            <p className='hero-para'>Our team of experts is eagerly waiting to assist you with the service. </p>
+            <div className="buttons"><button>Try for free!</button>
+            <button>SIGN-IN</button></div>
+            
         </div>
       </div>
     </div>
