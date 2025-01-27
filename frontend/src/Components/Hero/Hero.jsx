c6201231a8978fe4db0ea6afb53ca709390856d2
Raviteja5469 <ravitejaseguri@gmail.com>
2025-01-27 23:25:46 +0530

    modified the hero

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -5,14 +5,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero_container">
+      <div className="hero">
       <p className="hero-heading">
-        Explore Our Site for Hassle-Free Billing and Comprehensive Accounting
-        Services      </p>
-      <button className="explore-button" onClick={()=> {console.log("explore button clicked")}}>
-        <p>Explore More</p> <FontAwesomeIcon className="arrow-icon" icon={faArrowRight} />
-      </button>
+        <h1>Explore Our Site for Hassle-Free Billing and Comprehensive Accounting
+          Services</h1>
+        <h2>
+          Billing and accounting, made simpler: a one-stop solution that helps you pay, get paid, invoice, manage finances, track expenses, find financing & optimize cash flow.
+        </h2>
+      </p>
+      </div>
+      <div className="hero-images">
+        <img className="hero_img_1" src="/src/assets/hero_img_2.jpg" alt="" />
+        <button className="explore-button" onClick={() => { console.log("explore button clicked") }}>
+          <p>Explore More</p> <FontAwesomeIcon className="arrow-icon" icon={faArrowRight} />
+        </button>
+        <img className="hero_img_2" src="/src/assets/hero_img_1.jpg" alt="" />
+      </div>
+
     </div>
+
   );
 };
 
--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "./Navbar.css";
-import logo from "/src/assets/logo.png";
+import logo from "/src/assets/logo1.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
 import { Link, Outlet } from "react-router-dom";
